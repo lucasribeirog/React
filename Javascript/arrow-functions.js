08952c48f598a76be0f14923526d1c6b8b2d5c0e
lucasribeirog <lucas-ribeiro.silva@unesp.br>
2023-02-26 20:55:28 -0300

    arrow-functions

---
+++ Javascript/arrow-functions.js
@@ -0,0 +1,13 @@
+const printMyNameWithArrowFunction = (name) =>
+{
+    console.log(name);
+}
+
+printMyNameWithArrowFunction('Lucas with arrow functions');
+
+function printMyNameWithoutArrowFunction(name)
+{
+    console.log(name);
+}
+
+printMyNameWithoutArrowFunction('Lucas without arrow functions');
