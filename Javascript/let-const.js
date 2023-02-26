f90096962cd0e52f3ab1914e58f8e4bda5066eb8
lucasribeirog <lucas-ribeiro.silva@unesp.br>
2023-02-26 20:46:00 -0300

    let-and-const

---
+++ Javascript/let-const.js
@@ -0,0 +1,14 @@
+//Case of type of myNameLet is variable
+//In this case the variable "myName" allow multiple values
+var myNameLet = "Lucas Ribeiro";
+console.log(myNameLet);
+
+myName = "João";
+console.log(myNameLet);
+
+//Caso of type myNameConst is not variable
+const myNameConst = "Lucas Const";
+console.log(myNameConst);
+
+myNameConst = "João Const";
+console.log(myNameConst)
